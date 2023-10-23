d0e4485c386838ed55fedc89ce62ad05074bf283
Lucas <lucasgefreitas@gmail.com>
2023-10-23 12:05:59 -0300

    My Change for profile

--- .vscode/assets/script.js
+++ .vscode/assets/script.js
@@ -8,10 +8,10 @@ const img = document.querySelector("#profile img")
 // substituir a img
     if (html.classList.contains ("light")) {
      // se tiver light mode add a img light
-        img.setAttribute("src", "./assets/assets/avatar-light.png")
+        img.setAttribute("src", "./assets/assets/WhatsApp Image 2023-10-23 at 11.44.58.jpeg")
    } else {
   // se tiver sem light mode manter a img normal
-img.setAttribute("src", "./assets/assets/avatar.png")
+img.setAttribute("src", "./assets/assets/WhatsApp Image 2023-10-23 at 11.44.58 (1).jpeg")
   }
  
 }
